60016dbefd205d43a18f1fd3b168676f5561d712
vikasjadhav123 <79581751+vikasjadhav123@users.noreply.github.com>
2021-02-26 16:02:55 +0530

    Some Changes

--- src/app/_services/post-data.service.ts
+++ src/app/_services/post-data.service.ts
@@ -8,18 +8,18 @@ import { Observable, of, pipe } from 'rxjs';
 })
 export class PostDataService {
 
-  employee: any = [  
-                     { id : 1, title : 'PrimeNG', discription : 'Desingning', date : '25/10/1996' },
-                     { id : 2, title : 'Angular', discription : 'Develoment', date : '01/10/2005' }
-                  ];
+  employee: any = [
+    { id: 1, title: 'PrimeNG', discription: 'Desingning', date: '25/10/1996' },
+    { id: 2, title: 'Angular', discription: 'Develoment', date: '01/10/2005' }
+  ];
 
-  constructor() {  }
+  constructor() { }
 
-  getAllemployeePosts() : Observable<any> {
+  getAllemployeePosts(): Observable<any> {
     return of(this.employee);
-}
+  }
 
-  submitdata(isAdd:any,form:any):Observable<any> {
+  submitdata(isAdd: any, form: any): Observable<any> {
     if (isAdd) {
       console.log(form.value);
       this.Addfun(form);
@@ -28,37 +28,37 @@ export class PostDataService {
       let emp = this.employee.find((s: any) => s.id == form.value.id);
       emp.title = form.value.title;
       emp.discription = form.value.discription;
-      emp.date = form.value.date;      
+      emp.date = form.value.date;
     }
     form.reset();
-   return of(null) ;
+    return of(null);
 
   }
 
-  Addfun(form:any){
+  Addfun(form: any) {
     let leg: any = { id: '', title: '', discription: '', date: '' };
-        leg.id = form.value.id;
-        leg.title = form.value.title;
-        leg.discription = form.value.discription;
-        leg.date = form.value.date;
+    leg.id = form.value.id;
+    leg.title = form.value.title;
+    leg.discription = form.value.discription;
+    leg.date = form.value.date;
 
-        this.employee.push(leg);
+    this.employee.push(leg);
 
   }
 
-  deleterecord(id:any): Observable<any> {
+  deleterecord(id: any): Observable<any> {
     let empIndex = this.employee.findIndex((s: any) => s.id == id);
 
-     this.employee.splice(empIndex, 1);
-     return of(null);
-    
+    this.employee.splice(empIndex, 1);
+    return of(null);
+
   }
 
-  edit(id:any,form:any): Observable<any> {
+  edit(id: any, form: any): Observable<any> {
 
-        let empIndex = this.employee.find((s: any) => s.id == id);
-       form.setValue(empIndex);
-       return of(null);
+    let empIndex = this.employee.find((s: any) => s.id == id);
+    form.setValue(empIndex);
+    return of(null);
 
   }
 }
